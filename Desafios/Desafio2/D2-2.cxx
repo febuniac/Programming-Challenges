c92d3d32176fc481b78c173948c1e4b5b8583cad
febuniac <febuniac@gmail.com>
2017-05-08 23:09:05 -0300

    Desafio2 finalizado

--- Desafios/Desafio2/D2-2.cxx
+++ Desafios/Desafio2/D2-2.cxx
@@ -3,69 +3,76 @@
 #include <vector>
 #include <map>
 #include <stack>
-#include <sstream>      // std::istringstream, std::ws
-void erdos(int p1,int p2){
-}
-int main () {
-  char str[256];//def de uma string (vai ser o nome do meu arquivo)
+#include <sstream>// std::istringstream, std::ws
 
+int main () {
+  std::ofstream out("saida_erdos.txt");//arquivo de saida
+  
+  /*Leitura de entrada*/
+  char str[256];//def de uma string (vai ser o nome do meu arquivo)
   std::cout << "Enter the name of an existing text file: ";//print para o user
   std::cin.get (str,256);//pega a resp como nome do arquivo
-
   std::ifstream input(str);  //abre o arquivo com "input" 
   std::string line;
-  char p;//criando p para ser o num de parametros recebido ainda como char
-  input >> p;
-  int num_of_people= p - '0';//transforma em int  
-  std::string authors;
-  std::vector<std::string> authors_vec;
-  std::vector<std::string> pairs_vec;
-  std::map<std::string, int> associa;
-  std::map<std::string,int>::iterator it = associa.begin();
 
-  std::getline(input, authors);//elimina linha em branco
-  for (int i = 0; i < (num_of_people); i++) {//lidar com o espaço;
+  /*Descobrindo o num de autores*/
+  std::string people;//criando people para ser o num de parametros recebido ainda como char
+  std::getline(input, people);
+  int num_of_people= atoi(people.c_str());;//transforma em int  
+  
+  std::string authors;
+  std::vector<std::string> authors_vec;//vetor de autores
+  std::vector<std::string> pairs_vec;//vetor de pares
+ 
+  /*Criando Vector de autores*/
+  for (int i = 0; i < (num_of_people); i++) {//for que cria vector com os autores em ordem
      std::getline(input, authors);
      authors_vec.push_back(authors);
-     //std::cout<<authors_vec[i]<<'\n'  ;
   }
+
+  /*Descobrindo o num de pares*/
+  std::string pairs;//criando pairs para ser o num de parametrso recebido ainda como char
+  std::getline(input, pairs);//elimina linha em branco
+  int num_of_pairs= atoi(pairs.c_str());//transforma em int 
   
-  /*Associando valores aos nomes*/
-  for (int j = 0; j < num_of_people; j++) {
-    associa[authors_vec[j]]=j;//associando dentro do mapa os chars aos ints
-    //std::cout<<authors_vec[j]<<'\n';
 
-  }
-  for (it=associa.begin(); it!=associa.end(); ++it){
-        std::cout << it->first << " => " << it->second << '\n';
-      }
-
-
-  char pairs;//criando ch para ser o num de parametrso recebido ainda como char
-  input >> pairs;
-  int num_of_pairs= pairs - '0';//transforma em int 
-
+  int p1;//digito esquerdo do par
+  int p2;//digito direito do par
   std::string pair_1;
   std::string pair_2;
-  std::getline(input, pair_1);//elimina linha em branco
-  for (int j = 0; j < (num_of_pairs*2); j++) {//lidar com o espaço
+  /*Criando vector de distâncias de Erdos e prenchendo com -1*/
+  std::vector<int> dist_erdos;//criaçõa do vector que pega as distancias de erdos
+  for (int z = 0; z < num_of_people; z++){
+    dist_erdos.push_back(-1);//prenche com -1
+  }
+
+  /*Escrevendo no arquivo de saída*/
+  dist_erdos[0]=0;//valor inicial
+  out<<num_of_people<<'\n';//printa o num de autores no arquivo final
+  out<<authors_vec[0]<<":"<< dist_erdos[0]<<'\n';//printa o Erdos no arquivo final
+ 
+  /*prints*/
+  std::cout<<num_of_people<<'\n';
+  std::cout<<authors_vec[0]<<":"<< dist_erdos[0]<<'\n';
+  
+  
+  for (int j = 0; j < (num_of_pairs*2); j++) {
     input>>pair_1;
     input>>pair_2;
-    int p1 = atoi(pair_1.c_str());
-    int p2 = atoi(pair_2.c_str());
-    //std::getline(input, pair);
-    pairs_vec.push_back(pair_1);
+    p1 = atoi(pair_1.c_str());//tranformando de string p int
+    p2 = atoi(pair_2.c_str());//tranformando de string p int   
+    pairs_vec.push_back(pair_1);//adicionando no vector de pares
     pairs_vec.push_back(pair_2);
-    std::cout<<pairs_vec[j]<<'\n';
+    
+    if (dist_erdos[p2]==-1){//formula de erdos
+      dist_erdos[p2]=dist_erdos[p1]+1;//regra
+      std::cout << authors_vec[p2]<< ":" <<dist_erdos[p2]<<'\n';
+      out<< authors_vec[p2]<< ":" <<dist_erdos[p2]<<'\n';//printando no arquivo
+    }
   }
-  
+    input.close();
+    out.close();
     return 0;
 }
 
-/*
- for (int j = 0; j < (num_of_pairs*2); j++) {//lidar com o espaço
-  input >> pairs;
-  pairs_vec.push_back(pairs);
-  std::cout<<pairs_vec[j]<<'\n';
-  }
-  */
+
--- Desafios/Desafio2/D2.cxx
+++ Desafios/Desafio2/D2.cxx
@@ -3,52 +3,60 @@
 #include <vector>
 #include <map>
 #include <stack>
+
 int main () {
+  /*dados de saída*/
   int answer;//variavel que irá guardar a saida de cada caso.
   std::ofstream out("saida.txt");//leitura do arquivo de saida
+  
+  /*criação da pilha*/
   std::stack<int> pilha;// criando uma stack de nome pilha vazia
   std::cout << "tamanho da pilha: " << pilha.size() << '\n';//mostra o tamanho da pilha
-
+  
+  /*Dados para leitura de arquivo de entrada*/
   char str[256];//def de uma string (vai ser o nome do meu arquivo)
-
   std::cout << "Enter the name of an existing text file: ";//print para o user
   std::cin.get (str,256);//pega a resp como nome do arquivo
-
   std::ifstream is(str);  //abre o arquivo com "is"   
+  
+  /*criação do mapa*/
   std::map<char, int> associa;//cria um mapa(tipo dicionário) que associa um char à um int
 
+  //std::string ch;//criando people para ser o num de parametros recebido ainda como char
+  //std::getline(is, ch);//colocando o primeiro digito do arquivo de entrada em ch
+  //int number_of_letters= atoi(ch.c_str());;//transforma em t
 
+  /*Pegando o numero de letras*/
   char ch;//criando ch para ser o num de parametros recebido ainda como char
   is >> ch;//colocando o primeiro digito do arquivo de entrada em ch
   int number_of_letters = ch - '0';//transforma em int
   
-  
+  /*Variáveis*/
   char caracter;
   int inteiro;
   std::vector<char> letters_vec;//vetor que guarda as letras
+  char posic;
+  int elem_1;
+  int elem_2;
+  int operat;
   
-
+  /*Preenchendo vector de letras*/
   for (int i = 0; i < number_of_letters; i++) {
     is >> caracter;//pegando as letras
     letters_vec.push_back(caracter);//colocando as letras em um vector  
   }
   
+  /*inicializa o mapa*/
   std::map<char,int>::iterator it = associa.begin();//inicializando o mapa
 
-
-
+  /*pegando a operação como string*/
   std::string operation;//pegando a operaçao interia como uma string de uma linha
   is >> operation;//colocando a linha de operação em operation
-
+  
   int num_combinations;
   is >> num_combinations;//vendo as possiveis combinacoes (ex: 000,001...)
   out<<num_combinations<<'\n';//colocando este valor no aqruivo de saida
 
-  
-  char posic;
-  int elem_1;
-  int elem_2;
-  int operat;
 
  while (num_combinations !=  0){
 
@@ -56,6 +64,7 @@ int main () {
       is >> inteiro;//pegando os inteiros e associando as letras colocando isto no map
       associa[letters_vec[j]]=inteiro;//associando dentro do mapa os chars aos ints
     }
+
     /*Printando o Mapa*/
     std::cout << "nova combinação do mapa"<<'\n';
     for (it=associa.begin(); it!=associa.end(); ++it){
@@ -64,7 +73,6 @@ int main () {
 
     for(std::string::size_type i = 0; i < operation.size(); ++i) {
       posic = operation[i];//pegando cada char da operação que é uma string
-    
       switch (posic) {
         case '&':
           elem_1 = pilha.top();//salva o primeiro da pilha em elem_1
@@ -73,7 +81,6 @@ int main () {
           pilha.pop();//remove este valor
           operat = elem_1 & elem_2;//faz a operação
           pilha.push(operat);//coloca o resultado na pilha
-          
           break;
         case '^':
           elem_1 = pilha.top();
@@ -82,7 +89,6 @@ int main () {
           pilha.pop();
           operat = elem_1 ^ elem_2;
           pilha.push(operat);
-          
           break;
         case '|':
           elem_1 = pilha.top();
@@ -91,15 +97,12 @@ int main () {
           pilha.pop();
           operat = elem_1 | elem_2;
           pilha.push(operat);
-          
           break;
         default:
           pilha.push(associa[posic]);//coloca o valor da letra na pilha
-          break;
-          
-          
+          break; 
+        }
       }
-    }
   answer = pilha.top();//pega a resposta
   pilha.pop();//remove da pilha
   out << answer<<'\n';  //escreve no arquivo de saida,
@@ -107,7 +110,5 @@ int main () {
   }      
   is.close();//fecha input
   out.close();//fecha output
-
-
   return 0;
 }
